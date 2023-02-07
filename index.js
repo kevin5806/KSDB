6c34e0b87c05262b5f0277bc8442a01e17e087c3
kevin5806 <68478032+kevin5806@users.noreply.github.com>
2023-02-07 12:07:23 +0100

    static

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //serve per servire file statici accessibili da tutte le pagina del sito
-const __static =  __dirname + '/public';
+const __static =  __dirname + '/static';
 
 app.use(express.static(__static));
 
